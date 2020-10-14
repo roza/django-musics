d5da49c36465686a5ad2c0b853f5ac80b975be93
Gerard Rozsavolgyi <roza@univ-orleans.fr>
2020-10-14 22:13:42 +0200

    ajout template BS base

--- GestionMusiques/settings.py
+++ GestionMusiques/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'GestionMusiques.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
